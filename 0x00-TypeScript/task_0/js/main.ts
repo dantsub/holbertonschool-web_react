66bb97b656484e9a35013c4dd87c58cbc894a978
dantsub <dany.mrtinez@gmail.com>
2021-02-08 17:26:24 -0500

    Initial commit

---
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -0,0 +1,38 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+
+const student1: Student = {
+  firstName: 'Francisco',
+  lastName: 'Muñoz',
+  age: 23,
+  location: 'Bogotá'
+};
+const student2: Student = {
+  firstName: 'Angela',
+  lastName: 'Villa',
+  age: 29,
+  location: 'Medellín'
+};
+
+const studentsList: Student[] = [student1, student2];
+
+let tableHtml: string = `<table>
+  <tr>
+    <th>First name</th>
+    <th>Location</th>
+  </tr>
+`;
+
+studentsList.forEach(student => {
+  tableHtml += `<tr>
+    <td>${student.firstName}</td>
+    <td>${student.location}</td>
+  </tr>`
+});
+
+tableHtml += '</table>';
+document.write(tableHtml);
