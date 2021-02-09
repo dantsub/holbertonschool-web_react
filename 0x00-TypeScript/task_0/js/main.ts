6eaea5e096dcfca5693f1110bdc744516f43e785
dantsub <dany.mrtinez@gmail.com>
2021-02-09 15:24:26 -0500

    try a simple solution

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -20,20 +20,19 @@ const student2: Student = {
 
 const studentsList: Array<Student> = [student1, student2];
 
-const tableHtml: HTMLTableElement = document.createElement('table');
-const theadHtml: HTMLTableSectionElement = document.createElement('thead');
-const tbodyHtml: HTMLTableSectionElement = document.createElement('tbody');
-
-const rowHeader: HTMLTableRowElement = theadHtml.insertRow();
-rowHeader.insertCell(0).innerHTML = 'firstName';
-rowHeader.insertCell(1).innerHTML = 'location';
-
-tableHtml.append(theadHtml);
+let tableHtml: string = `<table>
+  <tr>
+    <th>First name</th>
+    <th>Location</th>
+  </tr>
+`;
 
 studentsList.forEach(student => {
-  const row: HTMLTableRowElement = tbodyHtml.insertRow();
-  row.insertCell(0).innerHTML = student.firstName;
-  row.insertCell(1).innerHTML = student.location;
+  tableHtml += `<tr>
+    <td>${student.firstName}</td>
+    <td>${student.location}</td>
+  </tr>`
 });
-tableHtml.append(tbodyHtml);
-document.body.appendChild(tableHtml);
+
+tableHtml += '</table>';
+document.write(tableHtml);
