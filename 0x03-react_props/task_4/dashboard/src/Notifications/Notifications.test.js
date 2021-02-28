8cd9096192fe845e7b80367e2c4dd6a7bd2f14af
dantsub <dany.mrtinez@gmail.com>
2021-02-27 19:54:39 -0500

    [Update] Notifications tests

--- 0x03-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
+++ 0x03-react_props/task_4/dashboard/src/Notifications/Notifications.test.js
@@ -22,4 +22,28 @@ describe('Test Notification.js', () => {
     expect(wrapper.find(NotificationItem).first().html()).to.eq('<li data-notification-type="default">New course available</li>');
     done();
   });
+
+  it('menu item is being displayed when displayDrawer is false', (done) => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('.menuItem')).to.have.lengthOf(1);
+    done();
+  });
+
+  it('Add a check that the div.Notifications is not being displayed when displayDrawer is false', (done) => {
+    const wrapper = shallow(<Notifications />);
+    expect(wrapper.find('.Notifications')).to.have.lengthOf(0);
+    done();
+  });
+
+  it('Add a check that the menu item is being displayed when displayDrawer is true', (done) => {
+    const wrapper = shallow(<Notifications displayDrawer={true} />);
+    expect(wrapper.find('.Notifications')).to.have.lengthOf(1);
+    done();
+  });
+
+  it('Add a check that the div.Notifications is being displayed when displayDrawer is true', (done) => {
+    const wrapper = shallow(<Notifications displayDrawer={true}/>);
+    expect(wrapper.find('.Notifications')).to.have.lengthOf(1);
+    done();
+  });
 });
