d0ce79bbb547481860e4f79ea7b4313a432afc70
dantsub <dany.mrtinez@gmail.com>
2021-03-14 15:36:34 -0500

    [Update] correction

--- 0x07-React_Immutable/2-nested.js
+++ 0x07-React_Immutable/2-nested.js
@@ -1,8 +1,5 @@
-import { formJS, isKeyed } from 'immutable';
+import { fromJS, getIn } from 'immutable';
 
 export default function accessImmutableObject(object, array) {
-  return formJS(object, (key, value, array) => {
-    console.log(key, value, array);
-    return isKeyed(value) ? value.toOrderedMap() : value.toList();
-  });
+  return getIn(fromJS(object), array, undefined);
 };
