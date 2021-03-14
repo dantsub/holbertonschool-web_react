d8c1e64c1a54cdceff1bb5288e6a3ab41df1e6f2
dantsub <dany.mrtinez@gmail.com>
2021-03-14 15:26:22 -0500

    [Update] try other import

--- 0x07-React_Immutable/1-map.js
+++ 0x07-React_Immutable/1-map.js
@@ -1,6 +1,4 @@
-import immutable from 'immutable';
-
-const { Map } = immutable;
+import { Map } from 'immutable';
 
 const getImmutableObject = (object) => Map(object);
 export default getImmutableObject;
