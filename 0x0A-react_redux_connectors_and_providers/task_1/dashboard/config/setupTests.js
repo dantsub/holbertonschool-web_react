74ff7ed547556bbabab92ea4fe315b6d3928c1b0
dantsub <dany.mrtinez@gmail.com>
2024-08-13 22:12:06 -0500

    Update react redux project

--- 0x0A-react_redux_connectors_and_providers/task_1/dashboard/config/setupTests.js
+++ 0x0A-react_redux_connectors_and_providers/task_1/dashboard/config/setupTests.js
@@ -1,8 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { afterEach } from "jest";
+import { cleanUp } from "@testing-library/react";
 
-Enzyme.configure({ adapter: new Adapter() });
+afterEach(() => {
+  cleanUp();
+})
--- 0x0A-react_redux_connectors_and_providers/task_1/dashboard/config/webpack.config.js
+++ 0x0A-react_redux_connectors_and_providers/task_1/dashboard/config/webpack.config.js
@@ -32,9 +32,8 @@ module.exports = {
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         use: [
-          "file-loader",
           {
-            loader: "image-webpack-loader",
+            loader: "file-loader",
             options: {
               bypassOnDebug: true, // webpack@1.x
               disable: true, // webpack@2.x and newer
